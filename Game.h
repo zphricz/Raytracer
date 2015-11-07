0009a62b8d17788e37cc6f0bc35eb22cb4132a1d
zphricz <zphricz@gmail.com>
2015-11-07 14:09:34 -0800

    -Code formatting

--- Game.h
+++ Game.h
@@ -13,76 +13,76 @@ constexpr float ERR_MARGIN = 0.0005;
 enum class GeometryType { NONE, SPHERE, QUAD, PLANE };
 
 struct ColorAccumulator {
-  float r;
-  float g;
-  float b;
-  uint32_t count;
-  std::atomic_flag flag;
-  void lock() {
-    while (flag.test_and_set(std::memory_order_acquire));
-  }
-  void unlock() {
-    flag.clear();
-  }
+    float r;
+    float g;
+    float b;
+    uint32_t count;
+    std::atomic_flag flag;
+    void lock() {
+        while (flag.test_and_set(std::memory_order_acquire))
+            ;
+    }
+    void unlock() { flag.clear(); }
 };
 
 class Game {
-private:
-  PerfSoftScreen *scr;
-  Linear::Vec3f top_left;
-  Linear::Vec3f dz;
-  Linear::Vec3f dx;
-  Linear::Vec3f dy;
-  float ratio;
-  bool running;
-  float plane_distance;
-  float pitch;
-  float yaw;
-  float fov;
-  float ambient_light_quantity;
-  float dof_blur_amount;
-  std::atomic<uint64_t> num_rays_cast;
-  Linear::Vec3f position;
-  std::vector<ColorAccumulator> accumulators;
-  std::vector<Sphere> spheres;
-  std::vector<Quad> quads;
-  std::vector<Plane> planes;
-  std::vector<Light> lights;
-  bool input_disabled;
+  private:
+    PerfSoftScreen *scr;
+    Linear::Vec3f top_left;
+    Linear::Vec3f dz;
+    Linear::Vec3f dx;
+    Linear::Vec3f dy;
+    float ratio;
+    bool running;
+    float plane_distance;
+    float pitch;
+    float yaw;
+    float fov;
+    float ambient_light_quantity;
+    float dof_blur_amount;
+    std::atomic<uint64_t> num_rays_cast;
+    Linear::Vec3f position;
+    std::vector<ColorAccumulator> accumulators;
+    std::vector<Sphere> spheres;
+    std::vector<Quad> quads;
+    std::vector<Plane> planes;
+    std::vector<Light> lights;
+    bool input_disabled;
 
-  void render_slice(int slice);
-  void handle_input();
-  void calculate_quad_normals();
+    void render_slice(int slice);
+    void handle_input();
+    void calculate_quad_normals();
 
-  float
-  detect_triangle_hit(Linear::Vec3f origin, Linear::Vec3f ray,
-                      int &triangle_index,
-                      float max_distance = std::numeric_limits<float>::max(),
-                      float cutoff = ERR_MARGIN);
-  float detect_quad_hit(Linear::Vec3f origin, Linear::Vec3f ray,
-                        int &quad_index,
+    float
+    detect_triangle_hit(Linear::Vec3f origin, Linear::Vec3f ray,
+                        int &triangle_index,
                         float max_distance = std::numeric_limits<float>::max(),
                         float cutoff = ERR_MARGIN);
-
-  float detect_plane_hit(Linear::Vec3f origin, Linear::Vec3f ray,
-                         int &plane_index,
-                         float max_distance = std::numeric_limits<float>::max(),
-                         float cutoff = ERR_MARGIN);
-
-  float
-  detect_sphere_hit(Linear::Vec3f origin, Linear::Vec3f ray, int &sphere_index,
+    float
+    detect_quad_hit(Linear::Vec3f origin, Linear::Vec3f ray, int &quad_index,
                     float max_distance = std::numeric_limits<float>::max(),
                     float cutoff = ERR_MARGIN);
-  float detect_hit(Linear::Vec3f origin, Linear::Vec3f ray, int &index,
-                   GeometryType &hit_type);
-  float detect_hit(Linear::Vec3f origin, Linear::Vec3f ray);
-  void ray_trace(Linear::Vec3f origin, Linear::Vec3f ray, float &r, float &g,
-                 float &b, float weight = 1.0, int depth = 0);
 
-public:
-  Game(PerfSoftScreen *scr);
-  ~Game();
-  void run();
+    float
+    detect_plane_hit(Linear::Vec3f origin, Linear::Vec3f ray, int &plane_index,
+                     float max_distance = std::numeric_limits<float>::max(),
+                     float cutoff = ERR_MARGIN);
+
+    float
+    detect_sphere_hit(Linear::Vec3f origin, Linear::Vec3f ray,
+                      int &sphere_index,
+                      float max_distance = std::numeric_limits<float>::max(),
+                      float cutoff = ERR_MARGIN);
+    float detect_hit(Linear::Vec3f origin, Linear::Vec3f ray, int &index,
+                     GeometryType &hit_type);
+    float detect_hit(Linear::Vec3f origin, Linear::Vec3f ray);
+    void ray_trace(Linear::Vec3f origin, Linear::Vec3f ray, float &r, float &g,
+                   float &b, float weight = 1.0, int depth = 0);
+
+  public:
+    Game(PerfSoftScreen *scr);
+    ~Game();
+    void run();
 };
 
 #endif
--- SceneComponents.h
+++ SceneComponents.h
@@ -3,39 +3,39 @@
 
 // Geometry
 struct Sphere {
-  Linear::Vec3f position;
-  float radius;
-  float reflectivity;
-  float spec_power;
-  SDL_Color c;
+    Linear::Vec3f position;
+    float radius;
+    float reflectivity;
+    float spec_power;
+    SDL_Color c;
 };
 
 struct Plane {
-  Linear::Vec3f p;
-  Linear::Vec3f normal;
-  float reflectivity;
-  float spec_power;
-  SDL_Color c;
+    Linear::Vec3f p;
+    Linear::Vec3f normal;
+    float reflectivity;
+    float spec_power;
+    SDL_Color c;
 };
 
 struct Quad {
-  Linear::Vec3f p1;
-  Linear::Vec3f p2;
-  Linear::Vec3f p3;
-  Linear::Vec3f p4;
-  Linear::Vec3f normal;
-  float reflectivity;
-  float spec_power;
-  SDL_Color c;
+    Linear::Vec3f p1;
+    Linear::Vec3f p2;
+    Linear::Vec3f p3;
+    Linear::Vec3f p4;
+    Linear::Vec3f normal;
+    float reflectivity;
+    float spec_power;
+    SDL_Color c;
 };
 
 // Lights are modelled as completely transparent spheres that emit light in
 // all directions from all points within the sphere
 struct Light {
-  Linear::Vec3f position;
-  float intensity;
-  float radius;
-  // SDL_Color c;
+    Linear::Vec3f position;
+    float intensity;
+    float radius;
+    // SDL_Color c;
 };
 
 #endif
